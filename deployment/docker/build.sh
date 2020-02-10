81e1a21499a5dfad66911d2bef27578f08838a81
kennych <kennych@cisco.com>
2020-02-10 10:03:41 +0800

    update build command for sbt build command

--- deployment/docker/build.sh
+++ deployment/docker/build.sh
@@ -4,7 +4,8 @@ echo "build Glance Jar Deployment Package"
 cd ../..
 rm -rf ./deployment/docker/glancescala/glance.jar
 
-./activator assembly
+#./activator assembly
+sbt assembly
 cp ./target/scala-2.11/Glance-assembly-1.0-SNAPSHOT.jar ./deployment/docker/glancescala/glance.jar
 cd ./deployment/docker/
 
